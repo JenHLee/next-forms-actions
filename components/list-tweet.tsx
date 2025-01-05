2490d4f34a2e023d4eb9da571f154c32a8b93ed4
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2025-01-05 02:20:45 -0700

    edit

--- components/list-tweet.tsx
+++ components/list-tweet.tsx
@@ -11,14 +11,14 @@ export default function ListTweet({
     id,
     tweet,
     created_at,
-    Like,
+    like,
 }:TweetListProps) {
     return (
         <Link href={`/tweets/${id}`} className="flex gap-5">
           <div className="flex flex-col gap-1">
             <span className="text-lg">{tweet}</span>
             <span className="text-sm text-neutral-500">{formatToTimeAgo(created_at.toString())}</span>
-            <span className="text-lg font-semibold">{Like}</span>
+            <span className="text-lg font-semibold">{like}</span>
           </div>
         </Link>
       );
