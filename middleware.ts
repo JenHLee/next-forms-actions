7313ae3b2aabe32c81e696b9b6d2c824d8eea459
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2025-01-04 21:29:50 -0700

    middleware edit

--- middleware.ts
+++ middleware.ts
@@ -2,26 +2,32 @@ import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 interface Routes {
-    [key: string]: boolean;
+  [key: string]: boolean;
 }
 
 const publicOnlyUrls: Routes = {
-    "/login": true,
-    "/create-account": true,
-}
+  "/login": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-    const session = await getSession();
-    const exists = publicOnlyUrls[request.nextUrl.pathname]
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
 
-    if(!session.id) {
-        if(!exists) {
-            return NextResponse.redirect(new URL("/create-account", request.url))
-        }
-    }else {
-            // user logged in =>
-            if (exists){
-                return NextResponse.redirect(new URL("/", request.url))
-            }
-        }
+  // 세션이 없고, 요청 경로가 publicOnlyUrls에 없는 경우
+  if (!session.id) {
+    if (!exists && request.nextUrl.pathname !== "/login") {
+      return NextResponse.redirect(new URL("/login", request.url));
     }
+  } else {
+    // 사용자가 로그인한 상태에서 publicOnlyUrls 경로로 접근하면 리다이렉트
+    if (exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+}
+
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
